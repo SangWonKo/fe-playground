afa37a0c50a7832d1353e1bd404d7b90b9ce93ee
SangWonKo <swko1017@naver.com>
2022-06-29 23:33:54 +0900

    라우터 세팅 및 auth context provider 구현

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import PreAssignmentGuide from './pages/PreAssignmentGuide';
+import Router from './routes/Router';
 
 function App() {
-  return <PreAssignmentGuide />;
+  return <Router />;
 }
 
 export default App;
